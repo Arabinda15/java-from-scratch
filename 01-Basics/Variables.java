4cc4d62fa052c80f3145bf44218e0a15946d4c30
Arabinda15 <arabindpradhan.12@gmail.com>
2026-06-16 22:54:10 +0530

    Add Varible Program

---
+++ 01-Basics/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        String name = "Arabinda";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
