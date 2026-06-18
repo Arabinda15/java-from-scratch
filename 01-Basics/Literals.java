8c69396654f02482b28f03589f51a1970cb51e0b
Arabinda15 <arabindpradhan.12@gmail.com>
2026-06-18 19:50:39 +0530

    Add Literals Program

--- 01-Basics/Literals.java
+++ 01-Basics/Literals.java
@@ -2,14 +2,20 @@ public class Literals{
     public static void main(String[] args) {
         int age = 20;
         byte storage = 24;
+        short s1 = 1000; 
+        long l1 = 100000; //long inputs are written with 'l' or 'L'.
         char ch = 'A'; //Char inputs are written in ~ ' '.
         float f1 = 2.5f; //float inputs are written with 'f'.
         double d1 = 5.55; //double inputs are written with 'd' or as default.
+        boolean b1 = true; //boolean inputs are written with 'true' or 'false'.
         System.out.println("Age: " + age);
         System.out.println("Storage: " + storage);
+        System.out.println("Short: " + s1);
+        System.out.println("Long: " + l1);
         System.out.println("Character: " + ch);
         System.out.println("Float: " + f1);
         System.out.println("Double: " + d1);
+        System.out.println("Boolean: " + b1);
 
     }
 }
