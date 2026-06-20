c1d476f11057e40db3b14a2e22694d0e7caca980
Arabinda15 <arabindpradhan.12@gmail.com>
2026-06-20 11:35:43 +0530

    Add Keyboard_input Program

---
+++ 01-Basics/Keyboard_input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Keyboard_input{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println( "You are " + age + " years old.");
+        sc.close();
+    }
+}
