688ce13319899b5b0096e504f53a432d9cf59f33
Arabinda15 <arabindpradhan.12@gmail.com>
2026-06-27 21:03:33 +0530

    Add LogicalAndOperator_statement Program

---
+++ 03-Conditionals/LogicalAndOperator.java
@@ -0,0 +1,14 @@
+public class LogicalAndOperator {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        boolean hasLicense = true;
+
+        if (age >= 18 && hasLicense) {
+            System.out.println("You can drive.");
+        } else {
+            System.out.println("You cannot drive.");
+        }
+    }
+}
